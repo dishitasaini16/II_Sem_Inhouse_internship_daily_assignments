e5fe69ce0572f8e221c2bcb587d546de8d7f99f7
dishitasaini16 <jayasaini1981@gmail.com>
2026-07-12 15:57:05 +0530

    DAY6

---
+++ DAY6/script.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById("toggleBtn");
+
+btn.addEventListener("click", function(){
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        btn.innerHTML = "☀️ Light Mode";
+    }else{
+        btn.innerHTML = "🌙 Dark Mode";
+    }
+
+});

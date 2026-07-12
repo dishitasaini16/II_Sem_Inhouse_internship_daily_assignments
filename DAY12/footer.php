7d9aac9ab9d54f3f71f65b0e2070f5c06ba944a5
dishitasaini16 <jayasaini1981@gmail.com>
2026-07-12 20:53:51 +0530

    DAY12

---
+++ DAY12/footer.php
@@ -0,0 +1,6 @@
+<footer style="text-align:center; padding:15px; background:#f1f1f1; margin-top:20px;">
+    <p>&copy; <?php echo date("Y"); ?> My Website. All Rights Reserved.</p>
+</footer>
+
+</body>
+</html>     
